9ced93480ea1421aa67ef114390db186b1ab852e
Richard Lister <ric@burton-2.home>
2014-10-29 10:52:48 -0400

    goto last change with M-g M-g

---
+++ lisp/goto-chg-cfg.el
@@ -0,0 +1,5 @@
+(global-set-key (kbd "M-g M-g") 'goto-last-change)
+;; (global-set-key [(meta ?.)] 'goto-last-change)
+;; (global-set-key [(meta ?>)] 'goto-last-change-reverse)
+;; (global-set-key [(control ?,)] 'goto-last-change-reverse) ;I use this for shell popup
+;; (global-set-key [(meta ?>)] 'goto-last-change-reverse)
