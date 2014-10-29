a5c1bd9d0095e49b9113d93b37792ada006604f9
Richard Lister <ric@burton-2.home>
2014-10-29 10:48:38 -0400

    jump char

---
+++ lisp/jump-char-cfg.el
@@ -0,0 +1,6 @@
+;; jump-char from melpa
+(global-set-key (kbd "M-j")   'jump-char-forward)
+(global-set-key (kbd "M-S-J") 'jump-char-backward)
+
+;; then repeat char to keep going, ;/, for forw/back,
+;; and C-c C-c to switch to ace-jump with char
