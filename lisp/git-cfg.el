dfc7c750fc9aa37ad2722af109a7cf97e33a46bb
Richard Lister <ric@burton-2.local>
2014-10-29 18:07:56 -0400

    git config

---
+++ lisp/git-cfg.el
@@ -0,0 +1,12 @@
+(global-set-key (kbd "C-x g") 'magit-status) ;magit from melpa
+
+(eval-after-load 'magit-mode
+  '(progn
+     (add-hook
+      'magit-mode-hook
+      (lambda ()
+        (setq magit-save-some-buffers nil)      ;do not bug me about saving files on C-x g, esp. dirs
+        ))))
+
+(global-git-gutter-mode t)              ;git-gutter-fringe from melpa
+(defalias 'gh 'git-link)                ;install git-link from melpa; push to kill-ring the current line/region in github
