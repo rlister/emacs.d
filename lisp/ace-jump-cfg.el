f7a02ddf6d32251586405eb47f2b6e36163ae65b
Richard Lister <ric@burton-2.home>
2014-10-29 10:47:34 -0400

    ace jump

---
+++ lisp/ace-jump-cfg.el
@@ -0,0 +1,4 @@
+;; from melpa
+(define-key global-map (kbd "C-x j") 'ace-jump-char-mode) ;show all chars
+(define-key global-map (kbd "C-x w") 'ace-jump-word-mode) ;lines beginning w/char
+(define-key global-map (kbd "C-x l") 'ace-jump-line-mode) ;all lines, was count-lines-page
