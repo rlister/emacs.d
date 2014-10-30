e8a6783ad953481302050301c7565a3b9b99fe8a
Richard Lister <ric@burton-2.local>
2014-10-30 01:24:32 -0400

    go to matching parenthesis

---
+++ lisp/goto-matching-paren.el
@@ -0,0 +1,6 @@
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert the character typed."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t                    (self-insert-command (or arg 1))) ))
