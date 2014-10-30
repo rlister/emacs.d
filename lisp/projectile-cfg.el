17928a1373c306d60824fb2e264cd392d4d3461f
Richard Lister <ric@burton-2.local>
2014-10-29 22:02:09 -0400

    projectile config

---
+++ lisp/projectile-cfg.el
@@ -0,0 +1,10 @@
+;; (projectile-mode t)
+(projectile-global-mode)
+(setq projectile-remember-window-configs nil)
+
+;; (global-set-key (kbd "C-z C-f") 'projectile-find-file)
+;; (global-set-key (kbd "C-z f")   'projectile-find-file)
+;; (global-set-key (kbd "C-z b")   'projectile-switch-to-buffer)
+;; (global-set-key (kbd "C-z d")   'projectile-find-dir)
+;; (global-set-key (kbd "C-z D")   'projectile-dired)
+;; (global-set-key (kbd "C-z p")   'projectile-switch-project)
