9b41fbcde388d26cdbced2e9eb7d0b27da6e7071
Richard Lister <ric@burton-3.local>
2014-11-05 12:24:12 -0500

    apache-mode

---
+++ lisp/apache-cfg.el
@@ -0,0 +1,8 @@
+;; install from melpa
+;; do I need these?
+(autoload 'apache-mode "apache-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
+(add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+(add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
+(add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
+(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
