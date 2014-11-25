98d5e7014bd81191c24b134478417891bb1f0a96
Richard Lister <ric@burton-5.home>
2014-11-25 09:38:59 -0500

    working on godoc, flymake and run

--- lisp/golang-cfg.el
+++ lisp/golang-cfg.el
@@ -1,6 +1,52 @@
+;; these are also set in profile, but make sure all inferior processes get them
+(setenv "GOPATH" (concat (getenv "HOME") "/code/go"))
+;; (setenv "PATH" (concat (getenv "PATH") ":" "/extra/path/element"))
+(setq exec-path (append exec-path (list (expand-file-name (concat (getenv "GOPATH") "/bin")))))
+
 ;; go-mode from melpa
-(defun my-go-mode-hook () 
+(defun my-go-mode-hook ()
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (setq tab-width 2 indent-tabs-mode 1))
+  (setq tab-width 2 indent-tabs-mode 1)
+  ;; (if (not (string-match "go" compile-command))
+  ;;     (set (make-local-variable 'compile-command)
+  ;;          "go build -v && go test -v && go vet"))
+	(set (make-local-variable 'compile-command)
+			 ;; "go build -v && go test -v && go vet")
+			 "go build -v")
+  ;; (local-set-key (kbd "M-.") 'godef-jump)) ; Godef jump key binding
+  (subword-mode)                        ;handle CamelCase
+  )
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
+
+;; godoc is installed to this homebrew location
+(add-to-list 'exec-path "/usr/local/opt/go/libexec/bin")
+
+(defun godoc (query)
+  "Show Go documentation for a query, much like M-x man."
+  (interactive (list (godoc--read-query)))
+  (unless (string= query "")
+    (set-process-sentinel
+     (start-process-shell-command "godoc" (godoc--get-buffer query)
+                                  (concat "/usr/local/opt/go/libexec/bin/godoc " query))
+     'godoc--buffer-sentinel)
+    nil))
+
+;; go get -u github.com/dougm/goflymake
+(add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
+(require 'go-flymake)
+(define-key go-mode-map (kbd "H-e") 'flymake-popup-current-error-menu)
+
+;; M-x compile
+(setq compilation-ask-about-save nil) ; M-x compile should not prompt for save
+(global-set-key (kbd "H-c") 'compile)
+
+;; (setq compilation-save-buffers-predicate
+;; 			(lambda ()
+;; 				(string-prefix-p my-compilation-root (file-truename (buffer-file-name)))))
+
+(defun go-run-current-file ()
+	"go run on current filename."
+	(interactive)
+	(shell-command (concat "go run " (buffer-file-name))))
+(define-key go-mode-map (kbd "H-r") 'go-run-current-file)
