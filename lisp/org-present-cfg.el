2f57f37873065014346759c933519f5bddeb6b77
Richard Lister <ric@burton-5.home>
2014-11-25 09:39:35 -0500

    hide cursor on presentation

--- lisp/org-present-cfg.el
+++ lisp/org-present-cfg.el
@@ -8,15 +8,18 @@
      (defalias 'op 'org-present)
 
      (setq org-present-mode-hook nil)
+     (setq org-present-mode-quit-hook nil)
      (add-hook 'org-present-mode-hook
                (lambda ()
-                 ;(org-present-big)
-                 (if (equal window-system 'ns)
+                 (org-present-big)
+                 (when (equal window-system 'ns)
                      (org-display-inline-images))
+                 (org-present-hide-cursor)
                  (org-present-read-only)))
 
      (add-hook 'org-present-mode-quit-hook
                (lambda ()
                  (org-remove-inline-images)
-                 (org-present-read-write)))
-     ))
+                 (org-present-show-cursor)
+                 (org-present-read-write)
+                 (org-present-small)))))
