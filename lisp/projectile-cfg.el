e9e5ac6d805abe280be7d5caa11b616b13bd6e19
Richard Lister <ric@burton-5.home>
2014-12-05 11:40:12 -0500

    make dedicated shell buffers unique to project

--- lisp/projectile-cfg.el
+++ lisp/projectile-cfg.el
@@ -1,4 +1,3 @@
-;; (projectile-mode t)
 (projectile-global-mode)
 (setq projectile-remember-window-configs nil)
 
@@ -8,3 +7,11 @@
 ;; (global-set-key (kbd "C-z d")   'projectile-find-dir)
 ;; (global-set-key (kbd "C-z D")   'projectile-dired)
 ;; (global-set-key (kbd "C-z p")   'projectile-switch-project)
+
+;; make a unique shell buffer for each project
+(defun shell-dedicated-toggle-projectile ()
+  "shell-dedicated-toggle with project-unique buffer name."
+  (interactive)
+  (shell-dedicated-toggle (format "*shell: %s*" (projectile-project-name))))
+
+(global-set-key (kbd "M-.") 'shell-dedicated-toggle-projectile) ;since iterm eats C-,
