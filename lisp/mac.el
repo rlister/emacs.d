42172aded477b9f777363ab2549d7097c800076c
Richard Lister <ric@burton-5.home>
2014-12-05 17:49:05 -0500

    kill-ring clipboard integration for emacs in tmux on mac

--- lisp/mac.el
+++ lisp/mac.el
@@ -11,3 +11,20 @@
   (setq ns-option-modifier 'super)    ;option key is Super
   (setq ns-function-modifier 'hyper)  ;fn key is Hyper
   )
+
+;; share terminal emacs kill-ring with OSX clipboard
+(when (eq system-type 'darwin)
+  (unless (display-graphic-p)
+    (when (and (> (length (getenv "TMUX")) 0) (executable-find "reattach-to-user-namespace"))
+
+    (defun paste-from-osx ()
+      (shell-command-to-string "reattach-to-user-namespace pbpaste") )
+
+    (defun cut-to-osx (text &optional push)
+      (let ((process-connection-type nil))
+        (let ((proc (start-process "pbcopy" "*Messages*" "reattach-to-user-namespace" "pbcopy") ))
+          (process-send-string proc text)
+          (process-send-eof proc))))
+
+      (setq interprogram-cut-function 'cut-to-osx)
+      (setq interprogram-paste-function 'paste-from-osx))))
