65cdcc80f29a06c339db7fd3b04b1f02023b0cb4
Richard Lister <ric@burton-5.home>
2014-12-08 23:14:26 -0500

    do not need emamux right now

--- lisp/tmux.el
+++ lisp/tmux.el
@@ -1,7 +1,7 @@
-;; wget https://raw.githubusercontent.com/syohex/emacs-emamux/master/emamux.el
-(add-to-list 'load-path "~/etc/emacs/pkg/emamux")
-(require 'emamux)
+;; ;; wget https://raw.githubusercontent.com/syohex/emacs-emamux/master/emamux.el
+;; (add-to-list 'load-path "~/etc/emacs/pkg/emamux")
+;; (require 'emamux)
 
-(global-set-key (kbd "C-z s") 'emamux:send-command)
-(global-set-key (kbd "C-z y") 'emamux:yank-from-list-buffers) ;yank from tmux buffer
-(global-set-key (kbd "C-z k") 'emamux:copy-kill-ring) ;copy kill-ring to tmux buffer
+;; (global-set-key (kbd "C-z s") 'emamux:send-command)
+;; (global-set-key (kbd "C-z y") 'emamux:yank-from-list-buffers) ;yank from tmux buffer
+;; (global-set-key (kbd "C-z k") 'emamux:copy-kill-ring) ;copy kill-ring to tmux buffer
