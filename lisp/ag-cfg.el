a3578a5219c7fdee6afcbd8e3727eb9b6fe39d05
Richard Lister <rlister+gh@gmail.com>
2015-02-05 13:07:25 -0500

    reuse current window for ag results instead of splitting window

--- lisp/ag-cfg.el
+++ lisp/ag-cfg.el
@@ -1,4 +1,5 @@
 ;; for ag 0.14+ which supports --color-match, add highlighting
 (eval-after-load "ag"
   '(progn
-     (setq ag-highlight-search t)))
+     (setq ag-highlight-search t)
+     (setq ag-reuse-window t)))
