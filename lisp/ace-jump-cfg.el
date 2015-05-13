d215c32d38d18400b08c00330c3554514b8ea70f
Richard Lister <rlister+gh@gmail.com>
2015-05-12 23:46:16 -0400

    limit ace-jump to current window

--- lisp/ace-jump-cfg.el
+++ lisp/ace-jump-cfg.el
@@ -2,3 +2,6 @@
 (define-key global-map (kbd "C-x j") 'ace-jump-char-mode) ;show all chars
 (define-key global-map (kbd "C-x w") 'ace-jump-word-mode) ;lines beginning w/char
 (define-key global-map (kbd "C-x l") 'ace-jump-line-mode) ;all lines, was count-lines-page
+
+;; just match in current window
+(setq ace-jump-mode-scope 'window)
