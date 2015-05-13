94c840a554dc45a2e6b6ea06b63c745406dbffac
Richard Lister <rlister+gh@gmail.com>
2015-05-12 23:50:49 -0400

    some tmux experiments

--- lisp/tmux.el
+++ lisp/tmux.el
@@ -5,3 +5,10 @@
 ;; (global-set-key (kbd "C-z s") 'emamux:send-command)
 ;; (global-set-key (kbd "C-z y") 'emamux:yank-from-list-buffers) ;yank from tmux buffer
 ;; (global-set-key (kbd "C-z k") 'emamux:copy-kill-ring) ;copy kill-ring to tmux buffer
+
+;; (defun emamux:switch-session ()
+;;   "Kill tmux session"
+;;   (interactive)
+;;   (emamux:check-tmux-running)
+;;   (let ((session (emamux:read-parameter-session)))
+;;     (emamux:tmux-run-command nil "switch" "-t" session)))
