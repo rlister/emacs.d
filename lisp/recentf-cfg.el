69aaf7777b158778a92ee2c67523cacb5ee1312b
Richard Lister <rlister+gh@gmail.com>
2015-06-03 23:43:48 -0400

    add recentf config

---
+++ lisp/recentf-cfg.el
@@ -0,0 +1,8 @@
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-x C-r") 'recentf-ido-find-file)
