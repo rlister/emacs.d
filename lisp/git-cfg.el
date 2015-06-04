f709809170ad2e93612704095f8dbd6ebdee3918
Richard Lister <rlister+gh@gmail.com>
2015-06-03 22:19:08 -0400

    tmp workaround for gh problem

--- lisp/git-cfg.el
+++ lisp/git-cfg.el
@@ -32,3 +32,13 @@
 
 (global-git-gutter-mode t)              ;git-gutter-fringe from melpa
 (defalias 'gh 'git-link)                ;install git-link from melpa; push to kill-ring the current line/region in github
+
+;; workaround until my pull-request is merged:
+;; https://github.com/sshaw/git-link/pull/19
+(defun git-link-new (link)
+  ;(message (kill-new link))
+  (kill-new link)
+  (message link)
+  (setq deactivate-mark t)
+  (when git-link-open-in-browser
+    (browse-url link)))
