de8d903cab5f51841d244f5438f94e67cda10fc6
Richard Lister <rlister+gh@gmail.com>
2015-06-07 18:25:21 -0400

    new, simplified mode-line (without powerline) for evil-mode states

--- lisp/mode-line.el
+++ lisp/mode-line.el
@@ -1,43 +1,10 @@
-(column-number-mode 1)             ;turn on column numbers in modeline
-(display-battery-mode 1)           ;turn on battery display in modeline
-
-;; (setq display-time-load-average t) ;display time and load average in modeline
-;; (setq display-time-load-average-threshold 0.0)
-;; (setq display-time-format "%a%d/%H:%M")
-;; (display-time-mode 1)
-
-;; (setq
-;;  mode-line-format
-;;  '(
-;;    mode-line-front-space
-;;    mode-line-mule-info
-;;    mode-line-client
-;;    mode-line-modified
-;;    mode-line-remote
-;;    mode-line-frame-identification
-;;    mode-line-buffer-identification
-;;    "   "
-;;    mode-line-position
-;;    (vc-mode vc-mode)
-;;    "  "
-;;    mode-line-modes
-;;    mode-line-misc-info
-;;    hvmode-line-end-spaces)
-
-;; e.g. use (mode-line-fill 'mode-line 10) "Hello world"
-;; (defun mode-line-fill (face reserve)
-;;   "Return empty space using FACE and leaving RESERVE space on the right."
-;;   (unless reserve
-;;     (setq reserve 20))
-;;   (when (and window-system (eq 'right (get-scroll-bar-mode)))
-;;     (setq reserve (- reserve 3)))
-;;   (propertize " "
-;;               'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
-;;               'face face))
-
-;; (setq-default mode-line-format
-;;               (list
-;;                " " mode-line-modified
-;;                " " mode-line-buffer-identification
-;;                " " mode-line-position
-;;                ))
+(setq-default
+ mode-line-format
+ (list
+  '(:eval evil-mode-line-tag)
+  '(:eval (propertize (format "[%s]" (projectile-project-name))'face 'font-lock-type-face))
+  " "
+  '(:eval (propertize "%b" 'face 'mode-line-buffer-id))
+  " " mode-line-modified " "
+  '(:eval (propertize "%m" 'face 'font-lock-comment-face))
+  )) 
