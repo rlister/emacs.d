96a687d4890690a9da1a217fe64c6a8702ff193d
Richard Lister <rlister+gh@gmail.com>
2015-06-08 00:07:51 -0400

    comments about .dict

--- lisp/ac-complete-cfg.el
+++ lisp/ac-complete-cfg.el
@@ -1,2 +1,6 @@
+;; autocomplete
+;; manual: http://auto-complete.org/doc/manual.html
+;; put my own completions in ~/.dict and call ac-clear-dictionary-cache to update
+
 (ac-config-default)
 (add-to-list 'ac-modes 'enh-ruby-mode)
