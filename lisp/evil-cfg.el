d7992d9cb337ce6abd16e870e0ca530d536dd401
Richard Lister <rlister+gh@gmail.com>
2015-06-25 23:18:00 -0400

    not sure I want these

--- lisp/evil-cfg.el
+++ lisp/evil-cfg.el
@@ -93,18 +93,18 @@
  "?" '(lambda () (interactive) (find-file "~/notes/vim.cheat"))
  "g" 'magit-status
  "o" 'occur
- "ci" 'evilnc-comment-or-uncomment-lines
- "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
- ;; "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
- "cc" 'evilnc-copy-and-comment-lines
- "cp" 'evilnc-comment-or-uncomment-paragraphs
- "cr" 'comment-or-uncomment-region
- "cv" 'evilnc-toggle-invert-comment-line-by-line
- "\\" 'evilnc-comment-operator
+ ;; "ci" 'evilnc-comment-or-uncomment-lines
+ ;; "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+ ;; ;; "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+ ;; "cc" 'evilnc-copy-and-comment-lines
+ ;; "cp" 'evilnc-comment-or-uncomment-paragraphs
+ ;; "cr" 'comment-or-uncomment-region
+ ;; "cv" 'evilnc-toggle-invert-comment-line-by-line
+ ;; "\\" 'evilnc-comment-operator
  )
 
 ;; get back to normal state
-(defalias 'vi 'evil-normal-state) 
+(defalias 'vi 'evil-normal-state)
 
 ;; in magit start insert mode when editing commit messages
 (evil-set-initial-state 'magit-log-edit-mode 'insert)
