e2189c9252ed58f179f0c5ddbc20ebeed82d2cc0
Richard Lister <rlister+gh@gmail.com>
2015-06-25 23:15:10 -0400

    end-of-code-or-line broken by evil, so switch to mwim instead

--- lisp/end-of-code-or-line.el
+++ lisp/end-of-code-or-line.el
@@ -1,25 +1,27 @@
 ;; make C-e move to end of line, then end before comment
 ;; from http://emacswiki.org/emacs/BackToIndentationOrBeginning
 
-(defun end-of-code-or-line () 
-   "Move to EOL. If already there, to EOL sans comments.
-    That is, the end of the code, ignoring any trailing comment
-    or whitespace.  Note this does not handle 2 character 
-    comment starters like // or /*.  Such will not be skipped."
-    (interactive)
-    (if (not (eolp))
-        (evil-end-of-line)
-      (skip-chars-backward " \t")
-      (let ((pt (point))
-            (lbp (line-beginning-position))
-            (lim))
-        (when (re-search-backward "\\s<" lbp t)
-          (setq lim (point))
-          (if (re-search-forward "\\s>" (1- pt) t)
-              (goto-char pt)
-            (goto-char lim)               ; test here ->
-          (while (looking-back "\\s<" (1- (point)))
-            (backward-char))
-          (skip-chars-backward " \t"))))))
+;; (defun end-of-code-or-line ()
+;;    "Move to EOL. If already there, to EOL sans comments.
+;;     That is, the end of the code, ignoring any trailing comment
+;;     or whitespace.  Note this does not handle 2 character
+;;     comment starters like // or /*.  Such will not be skipped."
+;;     (interactive)
+;;     (if (not (eolp))
+;;         (end-of-line)
+;;       (skip-chars-backward " \t")
+;;       (let ((pt (point))
+;;             (lbp (line-beginning-position))
+;;             (lim))
+;;         (when (re-search-backward "\\s<" lbp t)
+;;           (setq lim (point))
+;;           (if (re-search-forward "\\s>" (1- pt) t)
+;;               (goto-char pt)
+;;             (goto-char lim)               ; test here ->
+;;           (while (looking-back "\\s<" (1- (point)))
+;;             (backward-char))
+;;           (skip-chars-backward " \t"))))))
 
-(global-set-key (kbd "C-e") 'end-of-code-or-line) ;was move-end-of-line
+;; (global-set-key (kbd "C-e") 'end-of-code-or-line) ;was move-end-of-line
+
+;; above failed with evil-mode, so replaced with mwim installed from melpa (see evil-cfg)
