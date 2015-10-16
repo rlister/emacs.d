117582e46fa9eb3e8072339e1c30b9daae750c67
Richard Lister <rlister+gh@gmail.com>
2015-10-16 00:53:43 -0400

    cleanup some comments

--- init.el
+++ init.el
@@ -18,12 +18,10 @@
 (load-library "brew-theme")             ;my color theme
 (load-library "projectile-cfg")
 (load-library "mode-line")              ;setup modeline
-;(load-library "powerline-cfg")          ;use powerline libs
 
 (load-library "uniquify-cfg")           ;make buffer names unique
 (load-library "kill-ring-cfg")          ;M-y
 (load-library "insert-date-time")       ;M-x date
-;(load-library "safe-quit")              ;prompt to quit emacs
 (load-library "aliases")                ;some function shortcuts
 (load-library "whitespace-cfg")         ;setup whitespace handling
 (load-library "paren-cfg")              ;setup parentheses behaviour (inc smartparens)
@@ -32,21 +30,11 @@
 ;(load-library "toggle-selective-display-column") ;C-TAB
 
 ;;; movement
-;;(load-library "open-line-and-indent")     ;C-o
-;(load-library "beginning-or-indentation") ;C-a
-;; (load-library "end-of-code-or-line")      ;C-e (obsoleted by mwim)
-;; (load-library "ace-jump-cfg")             ;C-x {j,w,l}, also C-cC-c in jump-char
 (load-library "avy-cfg")                ;replaces ace-jump
-;;(load-library "jump-char-cfg")            ;jump to char with M-j
 (load-library "comment-region-or-line")   ;C-;
 ;(load-library "goto-line-with-feedback")  ;M-g g
-;;(load-library "dot-mode-cfg")             ;C-. emulates vi .
-;(load-library "goto-chg-cfg")             ;M-g M-g to goto last change
 (load-library "imenu-cfg")                ;M-m
-;;(load-library "expand-region-cfg")        ;expand region syntactically with C-]
-;;(load-library "mark-forward-sexp-cfg")    ;C-c[oOiI]
 ;(load-library "toggle-selective-display-column") ;code folding with C-TAB
-;;(load-library "visual-regexp-cfg")        ;C-c[rq]
 ;
 ;;; window movement
 (load-library "window-movement")        ;moving between windows
@@ -77,19 +65,3 @@
 ;;(load-library "puppet-cfg")             ;puppet-mode
 ;;(load-library "flycheck-cfg")           ;turn on globally
 (load-library "conf-cfg")               ;conf-mode
-;
-;;; services
-;(load-library "slack-cfg")
-;(load-library "haste-cfg")
-;;(load-library "erc-cfg")                ;C-c e for ERC
-;
-;;; (load-library "perspective-cfg")
-;(load-library "org-present-cfg")
-;
-;;; shells
-;(load-library "shell-dedicated")        ;M-i, M-,
-;;(load-library "multi-term-cfg")         ;C-z c
-;;; (load-library "shell-mode-cfg")         ;shell-mode extras
-;;(load-library "dired-cfg")              ;dired+ etc
-;
-;;; (toggle-frame-maximized)                ;start up maximized
