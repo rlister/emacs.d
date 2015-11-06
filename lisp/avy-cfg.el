b8319178a77880e00101282a6ee7eed72b0f921f
Richard Lister <rlister+gh@gmail.com>
2015-11-05 22:26:29 -0500

    jump to case given in avy

--- lisp/avy-cfg.el
+++ lisp/avy-cfg.el
@@ -2,3 +2,7 @@
 (setq avy-background t) ; gray background
 (setq avy-all-windows nil) ; should just show candidates from current window
 (setq avy-case-fold-search nil) ; do not ignore case
+
+;; jump to exact case given
+(setq avy-keys (number-sequence ?a ?z))
+;; (setq avy-keys (append (number-sequence ?a ?z) (number-sequence ?A ?Z)))
