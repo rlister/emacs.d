78a4b71b0b29f927c5eecc8151e9f958fc740c90
Richard Lister <rlister+gh@gmail.com>
2015-11-12 14:50:40 -0500

    when switching project, land on top-level dired

--- lisp/projectile-cfg.el
+++ lisp/projectile-cfg.el
@@ -1,12 +1,8 @@
 (projectile-global-mode)
-(setq projectile-remember-window-configs nil)
 
-;; (global-set-key (kbd "C-z C-f") 'projectile-find-file)
-;; (global-set-key (kbd "C-z f")   'projectile-find-file)
-;; (global-set-key (kbd "C-z b")   'projectile-switch-to-buffer)
-;; (global-set-key (kbd "C-z d")   'projectile-find-dir)
-;; (global-set-key (kbd "C-z D")   'projectile-dired)
-;; (global-set-key (kbd "C-z p")   'projectile-switch-project)
+(setq
+ projectile-switch-project-action 'projectile-dired ;when switching project land on top-level dired
+ projectile-remember-window-configs nil)
 
 ;; make a unique shell buffer for each project
 (defun shell-dedicated-toggle-projectile ()
