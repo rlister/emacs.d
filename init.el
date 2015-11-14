f1fc3a4bf4554a8635eb985d45819335c076c6a7
Richard Lister <rlister+gh@gmail.com>
2015-11-14 10:34:08 -0500

    un-vendor rainbow-mode as we now get it from elpa

--- init.el
+++ init.el
@@ -55,7 +55,6 @@
 (load-library "ruby-cfg")               ;ruby-mode
 (load-library "js-cfg")                 ;javascript settings
 (load-library "json-cfg")               ;json-mode and json-pretty-print
-;(load-library "rainbow-mode-cfg")       ;show colours
 (load-library "shell-script-cfg")       ;shell-script-mode
 ;(load-library "yaml-cfg")               ;yaml-mode
 ;(load-library "ag-cfg")                 ;ag, ag-project (silver searcher)
--- lisp/rainbow-mode-cfg.el
+++
@@ -1,2 +0,0 @@
-;; wget https://raw.github.com/emacsmirror/rainbow-mode/master/rainbow-mode.el
-(autoload 'rainbow-mode "rainbow-mode" nil t)
