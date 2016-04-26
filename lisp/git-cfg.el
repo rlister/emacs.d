00dab02cadcbf1568b3579a7072fcbdc49df486a
Richard Lister <rlister+gh@gmail.com>
2016-04-26 09:24:14 -0400

    fix bindings for timemachine in evil

--- lisp/git-cfg.el
+++ lisp/git-cfg.el
@@ -52,3 +52,9 @@
 ;;     (browse-url link)))
 
 ;; fix git-timemachine keys in evil
+;; see https://bitbucket.org/lyro/evil/issue/511/let-certain-minor-modes-key-bindings
+(eval-after-load 'git-timemachine
+  '(progn
+     (evil-make-overriding-map git-timemachine-mode-map 'normal)
+     ;; force update evil keymaps after git-timemachine-mode loaded
+     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))
