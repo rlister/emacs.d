8585ea7781024c16289da6ce99e429c94b65d520
Richard Lister <rlister+gh@gmail.com>
2016-07-06 22:05:28 -0400

    projectile switch to most recent open file, not dired

--- lisp/projectile-cfg.el
+++ lisp/projectile-cfg.el
@@ -1,13 +1,20 @@
 (projectile-global-mode)
 
+(defun ric-projectile-switch-to-last-buffer ()
+  "Switch to most recent file in project, to be called by projectile-switch-project-action."
+  (interactive)
+  (find-file (car (projectile-recentf-files))))
+
 (setq
- projectile-switch-project-action 'projectile-dired ;when switching project land on top-level dired
+ ;; projectile-switch-project-action 'projectile-dired
+ ;; projectile-switch-project-action 'projectile-recentf
+ projectile-switch-project-action 'ric-projectile-switch-to-last-buffer
  projectile-remember-window-configs nil)
 
 ;; make a unique shell buffer for each project
-(defun shell-dedicated-toggle-projectile ()
-  "shell-dedicated-toggle with project-unique buffer name."
-  (interactive)
-  (shell-dedicated-toggle (format "*shell: %s*" (projectile-project-name))))
+;; (defun shell-dedicated-toggle-projectile ()
+;;   "shell-dedicated-toggle with project-unique buffer name."
+;;   (interactive)
+;;   (shell-dedicated-toggle (format "*shell: %s*" (projectile-project-name))))
 
-(global-set-key (kbd "M-.") 'shell-dedicated-toggle-projectile) ;since iterm eats C-,
+;; (global-set-key (kbd "M-.") 'shell-dedicated-toggle-projectile) ;since iterm eats C-,
