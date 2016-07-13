43fb4dc1c8d3c3a13326df6bb6c99b49c046cb80
Richard Lister <rlister+gh@gmail.com>
2016-07-12 23:32:23 -0400

    comment out magit hackery while I try evil-magit

--- lisp/evil-cfg.el
+++ lisp/evil-cfg.el
@@ -118,42 +118,43 @@
 ;; get back to normal state
 (defalias 'vi 'evil-normal-state)
 
-;; in magit start insert mode when editing commit messages
-(evil-set-initial-state 'magit-log-edit-mode 'insert)
-(evil-set-initial-state 'git-commit-mode 'insert)
+;; commented out while I try evil-magit from melpa
+;; ;; in magit start insert mode when editing commit messages
+;; (evil-set-initial-state 'magit-log-edit-mode 'insert)
+;; (evil-set-initial-state 'git-commit-mode 'insert)
 
-;; use evil bindings in magit
-(evil-set-initial-state 'magit-status-mode 'motion)
-(evil-define-key 'motion magit-status-mode-map
-  "\C-f" 'evil-scroll-page-down
-  "\C-b" 'evil-scroll-page-up
-  ;; (kbd "RET") 'magit-visit-item
-  (kbd "RET") 'magit-diff-visit-file
-  (kbd "TAB") 'magit-section-toggle
-  "." 'magit-mark-item
-  "=" 'magit-diff-with-mark
-  "C" 'magit-add-log
-  "l" 'magit-log-popup
-  "I" 'magit-ignore-item-locally
-  "S" 'magit-stage-all
-  "U" 'magit-unstage-all
-  "X" 'magit-reset-working-tree
-  "d" 'magit-discard-item
-  "i" 'magit-ignore-item
-  "s" 'magit-stage-item
-  "u" 'magit-unstage-item
-  "z" 'magit-key-mode-popup-stashing
-  )
+;; ;; use evil bindings in magit
+;; (evil-set-initial-state 'magit-status-mode 'motion)
+;; (evil-define-key 'motion magit-status-mode-map
+;;   "\C-f" 'evil-scroll-page-down
+;;   "\C-b" 'evil-scroll-page-up
+;;   ;; (kbd "RET") 'magit-visit-item
+;;   (kbd "RET") 'magit-diff-visit-file
+;;   (kbd "TAB") 'magit-section-toggle
+;;   "." 'magit-mark-item
+;;   "=" 'magit-diff-with-mark
+;;   "C" 'magit-add-log
+;;   "l" 'magit-log-popup
+;;   "I" 'magit-ignore-item-locally
+;;   "S" 'magit-stage-all
+;;   "U" 'magit-unstage-all
+;;   "X" 'magit-reset-working-tree
+;;   "d" 'magit-discard-item
+;;   "i" 'magit-ignore-item
+;;   "s" 'magit-stage-item
+;;   "u" 'magit-unstage-item
+;;   "z" 'magit-key-mode-popup-stashing
+;;   )
 
-(evil-set-initial-state 'magit-blame-mode 'motion)
-(evil-define-key 'motion magit-blame-map
-  "q" 'magit-blame-mode ;toggle blame mode off
-  )
-(evil-set-initial-state 'magit-log-mode 'motion)
-(evil-define-key 'motion magit-log-mode-map
-  ;; "q" 'magit-blame-mode ;toggle blame mode off
-  (kbd "RET") 'magit-show-commit
-  )
+;; (evil-set-initial-state 'magit-blame-mode 'motion)
+;; (evil-define-key 'motion magit-blame-map
+;;   "q" 'magit-blame-mode ;toggle blame mode off
+;;   )
+;; (evil-set-initial-state 'magit-log-mode 'motion)
+;; (evil-define-key 'motion magit-log-mode-map
+;;   ;; "q" 'magit-blame-mode ;toggle blame mode off
+;;   (kbd "RET") 'magit-show-commit
+;;   )
 
 ;; evil motion for other modes
 (evil-set-initial-state 'package-menu-mode 'motion)
