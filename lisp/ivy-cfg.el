568ab57dd917802a7167d9f7777282d8a283c787
Richard Lister <rlister+gh@gmail.com>
2016-07-22 22:58:17 -0400

    use specfic counsel functions instead of binding ivy-mode
    This is due to a bug where ivy gets invoked in the wrong way on various
    completing and non-completing reads. Could be a timing issue.

--- lisp/ivy-cfg.el
+++ lisp/ivy-cfg.el
@@ -1,14 +1,18 @@
 ;; melpa swiper pkg also installs ivy and counsel
-(ivy-mode 1)
-(setq
- ;; ivy-use-virtual-buffers t ;add recentf and bookmark buffers to the list
- projectile-completion-system 'ivy   ;use ivy for all projectile stuff
- ivy-format-function 'ivy-format-function-arrow
- )
+;; (ivy-mode 1)
+(setq ivy-format-function 'ivy-format-function-arrow)
+
+;; use ivy for these modes
+(setq projectile-completion-system 'ivy)
+(setq magit-completing-read-function 'ivy-completing-read)
 
 ;; try this instead of smex
 (global-set-key (kbd "C-x m")   'counsel-M-x)  ;use this instead of M-x
 (global-set-key (kbd "C-x C-m") 'counsel-M-x)  ;common typo
 
 ;; instead of browse-kill-ring package
-(global-set-key (kbd "M-y") 'counsel-yank-pop)(global-set-key (kbd "M-m")     'counsel-imenu)
+(global-set-key (kbd "M-y")     'counsel-yank-pop)
+(global-set-key (kbd "C-h f")   'counsel-describe-function)
+(global-set-key (kbd "C-h v")   'counsel-describe-variable)
+(global-set-key (kbd "M-m")     'counsel-imenu)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
