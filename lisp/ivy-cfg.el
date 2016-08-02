a7dd2f38b94570734bb589dec363f5a742f5c620
Richard Lister <rlister+gh@gmail.com>
2016-08-01 21:08:40 -0400

    stupid company tricks

--- lisp/ivy-cfg.el
+++ lisp/ivy-cfg.el
@@ -16,3 +16,13 @@
 (global-set-key (kbd "C-h v")   'counsel-describe-variable)
 (global-set-key (kbd "M-m")     'counsel-imenu)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
+(defun ric-indent-or-complete ()
+  "Use TAB to call indent or counsel-company."
+  (interactive)
+  (if (looking-at "\\_>")
+      (counsel-company)
+    (indent-according-to-mode)))
+(global-set-key (kbd "TAB") 'ric-indent-or-complete)
+(setq company-idle-delay nil)           ;turn off auto-popup after idle
+(setq company-frontends '())
