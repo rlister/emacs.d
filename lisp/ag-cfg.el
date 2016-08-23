99bf2ab7f2ab5a3f4eb01721ef913100a84202d3
Richard Lister <rlister+gh@gmail.com>
2016-08-23 18:53:26 -0400

    fix ag executable-finding problems

--- lisp/ag-cfg.el
+++ lisp/ag-cfg.el
@@ -1,5 +1,6 @@
 ;; for ag 0.14+ which supports --color-match, add highlighting
 (eval-after-load "ag"
   '(progn
+     (setq ag-executable "/usr/local/bin/ag")
      (setq ag-highlight-search t)
      (setq ag-reuse-window t)))
