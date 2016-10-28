92265e1ee097a0fde83c3d83e12e47871330c0c4
Richard Lister <rlister@gmail.com>
2016-10-27 22:47:49 -0400

    organize things better in init

--- init.el
+++ init.el
@@ -20,62 +20,60 @@
 (load-library "mac")                    ;some mac-specific settings
 (load-library "basics")                 ;basic display and key tweaks
 (load-library "brew-theme")             ;my color theme
-(load-library "gpg-cfg")                ;for epg and epa
 (load-library "term-cfg")
-;; (load-library "iterm-cfg")
-(load-library "ivy-cfg")                ;ivy completion lib
-(load-library "projectile-cfg")
-(load-library "mode-line")              ;setup modeline
-;; (load-library "hide-mode-line")
-(load-library "uniquify-cfg")           ;make buffer names unique
-;; (load-library "insert-date-time")       ;M-x date
-(load-library "aliases")                ;some function shortcuts
 (load-library "whitespace-cfg")         ;setup whitespace handling
-(load-library "paren-cfg")              ;setup parentheses behaviour (inc smartparens)
 
-;;; movement
-(load-library "comment-region-or-line")   ;C-;
-(load-library "imenu-cfg")                ;M-m
-;(load-library "toggle-selective-display-column") ;code folding with C-TAB
-;; packages
+;; install use-package and load this
 (load-library "package-cfg")            ;add melpa and initialize
+
+;; packages for swiper
 (load-library "swiper-cfg")
 (load-library "counsel-cfg")
 (load-library "ivy-cfg")
+
+;; packages for evil mode
 (load-library "mwim-cfg")               ;intelligent beg/end of line
 (load-library "evil-cfg2")              ;setup evil-mode
 (load-library "evil-surround-cfg")
 (load-library "vim-empty-lines-mode")   ;tilde for traling empty lines
 (load-library "key-chord-cfg")          ;jk binding
+
+;; packages for projectile and perspective
 (load-library "projectile-cfg")
 (load-library "persp-projectile-cfg")
 (load-library "mode-line")              ;setup modeline for evil
 (load-library "term-projectile-cfg")    ;manage terms per project
-(load-library "uniquify-cfg")           ;make buffer names unique
 
-;;; window movement
-(load-library "window-movement")        ;moving between windows
+;; other packages
+(load-library "uniquify-cfg")           ;make buffer names unique
 (load-library "smartparens-cfg")        ;setup parentheses behaviour (inc smartparens)
 ;; (load-library "aggressive-indent-cfg")  ;crazy
 
-;;; completion
-;; (load-library "ac-complete-cfg")
-(load-library "pabbrev-cfg")
-;;(load-library "discover-cfg")
+;; ;;; movement
+;; (load-library "imenu-cfg")                ;M-m
+;; ;(load-library "toggle-selective-display-column") ;code folding with C-TAB
 
-;;; programming modes
-(load-library "git-cfg")                ;magit-mode
-(load-library "ruby-cfg")               ;ruby-mode
-(load-library "js-cfg")                 ;javascript settings
-(load-library "json-cfg")               ;json-mode and json-pretty-print
-(load-library "shell-script-cfg")       ;shell-script-mode
-;(load-library "yaml-cfg")               ;yaml-mode
-(load-library "ag-cfg")                 ;ag, ag-project (silver searcher)
-(load-library "golang-cfg")             ;setup go-mode
-(load-library "nginx-cfg")              ;nginx-mode
-(load-library "terraform-cfg")          ;terraform-mode
-(load-library "elisp-cfg")              ;elisp hooks
-(load-library "conf-cfg")               ;conf-mode
+;; ;;; window movement
+;; (load-library "window-movement")        ;moving between windows
 
-(load-library "org-cfg")                ;todo lists, etc
-(put 'dired-find-alternate-file 'disabled nil)
+;; ;;; completion
+;; ;; (load-library "ac-complete-cfg")
+;; (load-library "pabbrev-cfg")
+;; ;;(load-library "discover-cfg")
+
+;; ;;; programming modes
+;; (load-library "git-cfg")                ;magit-mode
+;; (load-library "ruby-cfg")               ;ruby-mode
+;; (load-library "js-cfg")                 ;javascript settings
+;; (load-library "json-cfg")               ;json-mode and json-pretty-print
+;; (load-library "shell-script-cfg")       ;shell-script-mode
+;; ;(load-library "yaml-cfg")               ;yaml-mode
+;; (load-library "ag-cfg")                 ;ag, ag-project (silver searcher)
+;; (load-library "golang-cfg")             ;setup go-mode
+;; (load-library "nginx-cfg")              ;nginx-mode
+;; (load-library "terraform-cfg")          ;terraform-mode
+;; (load-library "elisp-cfg")              ;elisp hooks
+;; (load-library "conf-cfg")               ;conf-mode
+
+;; (load-library "org-cfg")                ;todo lists, etc
+;; (put 'dired-find-alternate-file 'disabled nil)
