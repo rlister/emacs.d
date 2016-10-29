3a9d8e7dc18d1dbcbdc49823cf7652bcf587832e
Richard Lister <rlister@gmail.com>
2016-10-29 13:48:20 -0400

    namespace linux code

--- lisp/linux.el
+++ lisp/linux.el
@@ -1,8 +1,5 @@
-(setq x11 (cond ((equal window-system 'x) t)))
+(setq ric-x11-p (cond ((equal window-system 'x) t)))
 
-(when x11
+(when ric-x11-p
   (set-face-attribute 'default nil :family "Inconsolata" :height 120 :weight 'normal :width 'normal)
   )
-
-;; (when (eq system-type 'gnu/linux)
-;;   )
