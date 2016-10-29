c7d186184c07ae0c2a407134c3c841556d60743e
Richard Lister <rlister@gmail.com>
2016-10-29 14:04:09 -0400

    rename file

--- lisp/ric-comment-dwim.el
+++ lisp/ric-comment-dwim.el
@@ -1,8 +1,8 @@
-(defun comment-region-or-line (&optional arg)
+(defun ric/comment-dwim (&optional arg)
   "Toggle comments on region if active, otherwise toggle comment on line."
   (interactive "P")
   (if (region-active-p)
       (comment-or-uncomment-region (region-beginning) (region-end) arg)
     (comment-or-uncomment-region (line-beginning-position) (line-end-position) arg)))
 
-(global-set-key (kbd "C-z ;") 'comment-region-or-line)
+(global-set-key (kbd "C-z ;") 'ric/comment-dwim)
