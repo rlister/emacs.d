6908813ad0fe6c7eea84edd26a3b93d2243536eb
Richard Lister <rlister@gmail.com>
2016-10-31 16:57:29 -0400

    cleanup and move old code

--- lisp/expand-region-cfg.el
+++
@@ -1,7 +0,0 @@
-;; expand region by syntactic units
-;; git clone https://github.com/magnars/expand-region.el.git
-(add-to-list 'load-path '"~/etc/emacs/pkg/expand-region.el")
-(autoload 'er/expand-region "expand-region" nil t)
-
-(global-set-key (kbd "C-]") 'er/expand-region)
-(global-set-key (kbd "C-}") 'er/contract-region)
--- lisp/haste-cfg.el
+++
@@ -1,2 +0,0 @@
-;; git clone http://github.com/rlister/emacs-haste-client
-(autoload 'haste "haste" nil t)
