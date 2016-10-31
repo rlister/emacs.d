1460ad6895295516874c9117419c9d20fd446eaa
Richard Lister <rlister@gmail.com>
2016-10-31 00:26:16 -0400

    cleanup yaml mode config

--- lisp/yaml-cfg.el
+++ lisp/yaml-cfg.el
@@ -1,3 +1,7 @@
-;; yaml-mode from melpa
-(add-to-list 'auto-mode-alist '("\\.\\(yml\\|yaml\\|service\\|timer\\)$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.\\(yml\\|yaml\\)\\.erb$" . yaml-mode))
+(use-package yaml-mode
+  :ensure t
+  :mode (
+         "\\.yml\\'"
+         "\\.yml.erb\\'"
+         )
+  )
