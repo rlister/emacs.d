533cdbc0d4b2c11f2844d8e1775b990b94e5408d
Richard Lister <rlister@gmail.com>
2016-10-31 14:38:31 -0400

    cleanup

--- lisp/ace-jump-cfg.el
+++
@@ -1,7 +0,0 @@
-;; from melpa
-(define-key global-map (kbd "C-x j") 'ace-jump-char-mode) ;show all chars
-(define-key global-map (kbd "C-x w") 'ace-jump-word-mode) ;lines beginning w/char
-(define-key global-map (kbd "C-x l") 'ace-jump-line-mode) ;all lines, was count-lines-page
-
-;; just match in current window
-(setq ace-jump-mode-scope 'window)
