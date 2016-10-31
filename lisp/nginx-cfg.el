cb1fd6095c5e4c50d55102523845979e3ff14ee5
Richard Lister <rlister@gmail.com>
2016-10-31 00:26:29 -0400

    use mode for nginx

--- lisp/nginx-cfg.el
+++ lisp/nginx-cfg.el
@@ -1,6 +1,7 @@
 (use-package nginx-mode
+  :mode "config/nginx/.*"
   :config
   (setq nginx-indent-level 2)
-  (add-to-list 'auto-mode-alist
-               '("config/nginx/.*" . nginx-mode))
+  ;; (add-to-list 'auto-mode-alist
+  ;;              '("config/nginx/.*" . nginx-mode))
   )
