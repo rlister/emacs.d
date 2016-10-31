517cd4032fe9243801c16556021b7306b005516f
Richard Lister <rlister@gmail.com>
2016-10-30 23:13:11 -0400

    force chruby to load

--- lisp/chruby-cfg.el
+++ lisp/chruby-cfg.el
@@ -1,5 +1,7 @@
 (use-package chruby
-  :config
+  :ensure t                             ;cannot get this to lazy load properly
+  :init
   (require 'cl)                         ;chruby uses mapcan and needs this
   (chruby "2.3.1")
+  ;; (add-hook 'ruby-mode-hook 'chruby)
   )
