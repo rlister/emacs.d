8c74ea47ae3dd61f205905333ee2ff7312eba082
Richard Lister <rlister@gmail.com>
2016-10-30 23:02:34 -0400

    nicer bindong to open project files

--- lisp/counsel-projectile-cfg.el
+++ lisp/counsel-projectile-cfg.el
@@ -4,6 +4,7 @@
          ("C-z m"   . counsel-projectile-switch-project)
          ("C-z M"   . counsel-projectile-switch-project)
          ("C-z b"   . counsel-projectile-switch-to-buffer)
+         ([C-return] . counsel-projectile-find-file)
          ("C-x f"   . counsel-projectile-find-file)
          ("C-z f"   . counsel-projectile-find-file)
          ("C-z C-f" . counsel-projectile-find-file)
