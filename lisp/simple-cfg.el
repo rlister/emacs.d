96418372d6926d3e8b7d65def12f29cb322402f5
Richard Lister <rlister@gmail.com>
2016-10-31 22:40:44 -0400

    put selective display code with corresponding package

--- lisp/simple-cfg.el
+++ lisp/simple-cfg.el
@@ -3,4 +3,11 @@
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
   :bind
   ("C-m" . newline-and-indent) ;make RET indent
+  ("C-c TAB" . ric/toggle-selective-display-column)
   )
+
+(defun ric/toggle-selective-display-column ()
+  "Set selective display fold everything greater than the current column, or toggle off if active."
+  (interactive)
+  (set-selective-display
+   (if selective-display nil (or (+ (current-column) 1) 1))))
