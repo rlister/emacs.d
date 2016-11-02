87bfec11b69d191931857408acf77abe424e36e6
Richard Lister <rlister@gmail.com>
2016-11-02 10:24:04 -0400

    make sure path gets right emacs

--- bin/org-capture.sh
+++ bin/org-capture.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
 ## invoke a silent org-capture from shell
+
+## ensure we get correct emacsclient
+export PATH=/usr/local/bin:/usr/bin
+
+## escape text to make it URI-safe
 data=$(perl -MURI::Escape -e "print uri_escape('$*');")
+
+## send to org-mode
 emacsclient org-protocol://capture://x/${data}
