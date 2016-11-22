1408ba9c73c97286d4649e36c6318f79c8cfc4c4
Richard Lister <rlister@gmail.com>
2016-11-22 16:53:44 -0500

    ruby 2.3.3 for chruby

--- lisp/chruby-cfg.el
+++ lisp/chruby-cfg.el
@@ -2,6 +2,6 @@
   :ensure t                             ;cannot get this to lazy load properly
   :init
   (require 'cl)                         ;chruby uses mapcan and needs this
-  (chruby "2.3.1")
+  (chruby "2.3.3")
   ;; (add-hook 'ruby-mode-hook 'chruby)
   )
