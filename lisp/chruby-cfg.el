945659240c15d364d3ac6b69f270f47db7bd036b
Richard Lister <rlister@gmail.com>
2017-02-16 13:25:47 -0500

    bump chruby to 2.4.0

--- lisp/chruby-cfg.el
+++ lisp/chruby-cfg.el
@@ -2,6 +2,6 @@
   :ensure t                             ;cannot get this to lazy load properly
   :init
   (require 'cl)                         ;chruby uses mapcan and needs this
-  (chruby "2.3.3")
+  (chruby "2.4.0")
   ;; (add-hook 'ruby-mode-hook 'chruby)
   )
