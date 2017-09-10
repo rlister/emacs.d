63c732da4639e8b088f53c40ff7daf679d60a29b
Richard Lister <rlister@gmail.com>
2017-09-10 11:21:39 -0400

    theme preamble

--- lisp/brew-theme.el
+++ lisp/brew-theme.el
@@ -1,5 +1,31 @@
-(deftheme brew "Brew theme")
+;;; brew-theme.el --- Emacs color theme with named colors
 
+;; Copyright (C) 2016 , Ric Lister
+
+;; Author: Ric Lister
+;; Version: 0.1
+;; Package-Requires: ((emacs "24"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
+(deftheme brew "Brew theme")
 
 (let ((brew-fg  "azure4")
   (custom-theme-set-faces
