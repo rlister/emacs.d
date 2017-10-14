32a72542fcbfc0273715aff23a4bd9d454b3f1a3
Richard Lister <rlister@gmail.com>
2017-10-13 23:20:10 -0400

    update epa config with more info and use brew gnupg2

--- lisp/epa-cfg.el
+++ lisp/epa-cfg.el
@@ -4,8 +4,17 @@
 ;; as a workaround for now, install from https://gpgtools.org/
 ;; this integrates with keychain and gives graphical prompt
 ;; check back later for fix with gnupg
+
+;; 2017-10-13 updated gnupg2 2.2.1, but problem appears to
+;; be with default pinentry 1.1.0, which has same error.
+;; Workaround is to brew install pinentry-mac, and point
+;; to it in ~/.gnupg/gpg-agent.conf:
+;;   /usr/local/bin/pinentry-mac
+;; and pkill -SIGHUP gpg-agent
+
 (use-package epa
   :init
   (when ric-darwin-p
-    (setq epg-gpg-program "/usr/local/MacGPG2/bin/gpg2"))
+    ;; (setq epg-gpg-program "/usr/local/MacGPG2/bin/gpg2"))
+    (setq epg-gpg-program "/usr/local/bin/gpg"))
   )
