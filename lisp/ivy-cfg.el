e60795fbe6af1848c47451c70238d1f9227c377e
Richard Lister <rlister@gmail.com>
2017-10-27 17:55:43 -0400

    use this for buffer switching now

--- lisp/ivy-cfg.el
+++ lisp/ivy-cfg.el
@@ -4,6 +4,7 @@
   (setq projectile-completion-system 'ivy)
   (setq magit-completing-read-function 'ivy-completing-read)
   :bind (
+         ("<M-return>" . ivy-switch-buffer)
          ("C-x b" . ivy-switch-buffer)
          ("C-x B" . switch-to-buffer)   ;legacy
          ))
