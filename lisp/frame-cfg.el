f50774d5aff89ee4cf032f568ac6b38d9943519e
Richard Lister <ric@firefly.home>
2017-10-27 22:24:07 -0400

    turn off this binding

--- lisp/frame-cfg.el
+++ lisp/frame-cfg.el
@@ -1,6 +1,5 @@
 (use-package frame
   :bind
-  ;; ("<M-return>" . toggle-frame-fullscreen)
   ("<s-return>" . toggle-frame-fullscreen)
   :config
   (blink-cursor-mode -1)             ;or I will kill you
