b3131df03823a76a23db0ea5df47f45783d3e7fe
Richard Lister <rlister@gmail.com>
2018-07-10 10:31:12 -0400

    remove commented code

--- lisp/ivy-cfg.el
+++ lisp/ivy-cfg.el
@@ -9,12 +9,3 @@
          ("C-x B" . switch-to-buffer)   ;legacy
          ))
 
-;; (defun ric-indent-or-complete ()
-;;   "Use TAB to call indent or counsel-company."
-;;   (interactive)
-;;   (if (looking-at "\\_>")
-;;       (counsel-company)
-;;     (indent-according-to-mode)))
-;; (global-set-key (kbd "TAB") 'ric-indent-or-complete)
-;; ;; (setq company-idle-delay nil)           ;turn off auto-popup after idle
-;; ;; (setq company-frontends '())
