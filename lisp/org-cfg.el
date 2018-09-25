240acb72c4914b9194133a62123ba4004b904df1
Richard Lister <rlister@gmail.com>
2018-09-24 23:01:08 -0400

    experimenting with custom agendas

--- lisp/org-cfg.el
+++ lisp/org-cfg.el
@@ -16,6 +16,24 @@
                            "~/notes/gtd/gtd.org"
                            "~/notes/gtd/tickler.org"
                            )
+        org-agenda-custom-commands '(
+                                     ("w" "Agenda and work tasks"
+                                      ((agenda "" ((org-agenda-span 7)
+                                                   (org-agenda-start-on-weekday nil)))
+                                       (tags-todo "work")))
+                                     ("h" "Agenda and home tasks"
+                                      ((agenda "" ((org-agenda-span 7)
+                                                   (org-agenda-start-on-weekday nil)))
+                                       (tags-todo "home")))
+                                     ;; ((org-agenda-prefix-format " %i %-12:c %l"))
+                                     ;; )))
+                                     ;; (org-agenda-prefix-format)
+                                     ;; Its value is
+                                     ;; ((agenda . " %i %-12:c%?-12t% s")
+                                     ;;  (todo . " %i %-12:c")
+                                     ;;  (tags . " %i %-12:c")
+                                     ;;  (search . " %i %-12:c"))
+                                     )
         org-fontify-done-headline t
         org-todo-keywords '("TODO" "WAIT" "|" "DONE" "CANCELLED")
         org-todo-interpretation 'sequence
