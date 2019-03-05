fe4994dfc8e28cea244f891ebfe87f43eb224221
Richard Lister <rlister@gmail.com>
2019-03-05 11:13:21 -0500

    I have a higher res monitor now

--- lisp/linux.el
+++ lisp/linux.el
@@ -1,5 +1,5 @@
 (setq ric-x11-p (cond ((equal window-system 'x) t)))
 
 (when ric-x11-p
-  (set-face-attribute 'default nil :family "Roboto Mono" :height 140 :weight 'normal :width 'normal)
+  (set-face-attribute 'default nil :family "Roboto Mono" :height 110 :weight 'normal :width 'normal)
   )
