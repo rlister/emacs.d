dd9dffd717b5261af1484cbe8ee94965f99a2efb
Richard Lister <rlister@gmail.com>
2019-03-05 11:14:41 -0500

    extra binding for when C-z is clobbered

--- lisp/term-projectile-cfg.el
+++ lisp/term-projectile-cfg.el
@@ -1,6 +1,7 @@
 (use-package term-projectile
   :ensure t
   :bind
+  ("C-x c" . term-projectile-create-new)
   ("C-z c" . term-projectile-create-new)
   ("C-z n" . term-projectile-backward)
   ("C-z p" . term-projectile-forward)
