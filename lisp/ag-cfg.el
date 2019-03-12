03443ed8a8f478ac355693c43e4cf0e242178f13
Richard Lister <rlister@gmail.com>
2019-03-12 17:29:02 -0400

    make this multi-platform

--- lisp/ag-cfg.el
+++ lisp/ag-cfg.el
@@ -1,7 +1,7 @@
 (use-package ag
   :ensure t
   :config
-  (setq ag-executable "/usr/local/bin/ag")
+  (setq ag-executable "ag")
   (setq ag-highlight-search t)
   (setq ag-reuse-window t)
   :bind ("M-SPC" . ag-project)
