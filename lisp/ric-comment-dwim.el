58c5cd062b999dd6c6fa8a715f12d6d7808262e9
Richard Lister <rlister@gmail.com>
2019-03-12 16:59:46 -0400

    whitespace

--- lisp/ric-comment-dwim.el
+++ lisp/ric-comment-dwim.el
@@ -6,4 +6,4 @@
     (comment-or-uncomment-region (line-beginning-position) (line-end-position) arg)))
 
 (global-set-key (kbd "C-;") 'ric/comment-dwim)
-(global-set-key (kbd "C-z ;") 'ric/comment-dwim)
+;(global-set-key (kbd "C-z ;") 'ric/comment-dwim)
