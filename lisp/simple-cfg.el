cac9e64805a07f525d90c36084a390ac633ffafa
Richard Lister <rlister@gmail.com>
2019-03-12 17:04:35 -0400

    think I no longer use these

--- lisp/simple-cfg.el
+++ lisp/simple-cfg.el
@@ -2,12 +2,12 @@
   :config
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
   :bind
-  ("C-m" . newline-and-indent) ;make RET indent
-  ("C-c TAB" . ric/toggle-selective-display-column)
+  ;; ("C-m" . newline-and-indent) ;make RET indent
+  ;; ("C-c TAB" . ric/toggle-selective-display-column)
   )
 
-(defun ric/toggle-selective-display-column ()
-  "Set selective display fold everything greater than the current column, or toggle off if active."
-  (interactive)
-  (set-selective-display
-   (if selective-display nil (or (+ (current-column) 1) 1))))
+;; (defun ric/toggle-selective-display-column ()
+;;   "Set selective display fold everything greater than the current column, or toggle off if active."
+;;   (interactive)
+;;   (set-selective-display
+;;    (if selective-display nil (or (+ (current-column) 1) 1))))
