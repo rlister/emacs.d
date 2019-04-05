3a2d7f9c3abc8d8557ee18dd2dd4fb386d113d49
Richard Lister <rlister@gmail.com>
2019-04-05 11:15:55 -0400

    unbinding c-h

--- lisp/counsel-cfg.el
+++ lisp/counsel-cfg.el
@@ -5,8 +5,6 @@
          ("C-x m"   . counsel-M-x)
          ("C-x C-m" . counsel-M-x)
          ("M-y"     . counsel-yank-pop)
-         ("C-h f"   . counsel-describe-function)
-         ("C-h v"   . counsel-describe-variable)
          ("M-m"     . counsel-imenu)
          ("C-x C-f" . counsel-find-file)
          ("C-s"     . counsel-grep-or-swiper)
