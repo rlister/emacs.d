dea306d3fcef09b4e1dc3ccccc96d523a5c45f73
Richard Lister <rlister@gmail.com>
2019-09-29 11:54:41 -0400

    new bindings

--- lisp/counsel-projectile-cfg.el
+++ lisp/counsel-projectile-cfg.el
@@ -5,9 +5,9 @@
   (setq counsel-projectile-remove-current-buffer t)
   :bind (
          ("C-x p"      . (lambda () (interactive) (counsel-projectile-switch-project "D")))
+         ("C-,"        . (lambda () (interactive) (counsel-projectile-switch-project "D")))
          ("M-,"        . (lambda () (interactive) (counsel-projectile-switch-project "D")))
-         ("C-x <return>" . counsel-projectile-switch-to-buffer)
-         ("<M-return>" . counsel-projectile-switch-to-buffer)
-         ("<C-return>" . counsel-projectile-find-file)
+         ("C-<return>" . switch-to-buffer)
+         ("C-o"        . counsel-projectile-find-file)
          ("C-x C-d"    . counsel-projectile-find-dir)
          ))
