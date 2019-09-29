2713444670e4ac64d95ab01bd0bbcb0cc891dd39
Richard Lister <rlister@gmail.com>
2019-09-29 11:54:05 -0400

    use remaps in counsel bindings to make bindings more generic

--- lisp/counsel-cfg.el
+++ lisp/counsel-cfg.el
@@ -1,12 +1,13 @@
 (use-package counsel
   :ensure t
   :init
+  (define-key global-map [remap find-file] 'counsel-find-file)
+  (define-key global-map [remap yank-pop] 'counsel-yank-pop)
+  (define-key global-map [remap isearch-forward] 'counsel-grep-or-swiper)
+  (define-key global-map [remap describe-function] 'counsel-describe-function)
+  (define-key global-map [remap describe-variable] 'counsel-describe-variable)
   :bind (
+         ("C-SPC"   . counsel-M-x)
          ("C-x m"   . counsel-M-x)
          ("C-x C-m" . counsel-M-x)
-         ("M-y"     . counsel-yank-pop)
-         ("M-m"     . counsel-imenu)
-         ("C-x C-f" . counsel-find-file)
-         ("C-s"     . counsel-grep-or-swiper)
-         ;; ("C-S"     . isearch-forward)  ;legacy
          ))
