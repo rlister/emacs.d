6eac870e411b95c0635227998a51498e51edb54a
Richard Lister <rlister@gmail.com>
2019-09-29 11:56:30 -0400

    turn off transformer in favor of ivy-rich handling

--- lisp/ivy-cfg.el
+++ lisp/ivy-cfg.el
@@ -3,40 +3,35 @@
   :init
   (setq projectile-completion-system 'ivy)
   (setq magit-completing-read-function 'ivy-completing-read)
-  (ivy-set-display-transformer
-   'ivy-switch-buffer 'ric/ivy-switch-buffer-transformer)
-  :bind (
-         ;; ("<M-return>" . ivy-switch-buffer)
-         ("C-x b" . ivy-switch-buffer)
-         ("C-x B" . switch-to-buffer)   ;legacy
-         ))
+  ;; (ivy-set-display-transformer
+  ;;  'ivy-switch-buffer 'ric/ivy-switch-buffer-transformer)
 
-(defalias 'ric/ivy--term-p
-  (lambda (name)
-    "Return non-nil if buffer NAME looks like a term."
-    (string-match-p "\\`term -" name)))
+;; (defalias 'ric/ivy--term-p
+;;   (lambda (name)
+;;     "Return non-nil if buffer NAME looks like a term."
+;;     (string-match-p "\\`term -" name)))
 
-(defalias 'ric/ivy--ruby-p
-  (lambda (name)
-    "Return non-nil if buffer NAME looks like a ruby file."
-    (string-match-p "\\.rb\\b" name)))
+;; (defalias 'ric/ivy--ruby-p
+;;   (lambda (name)
+;;     "Return non-nil if buffer NAME looks like a ruby file."
+;;     (string-match-p "\\.rb\\b" name)))
 
-(defalias 'ric/ivy--js-p
-  (lambda (name)
-    "Return non-nil if buffer NAME looks like a javascript file."
-    (string-match-p "\\.js\\b" name)))
+;; (defalias 'ric/ivy--js-p
+;;   (lambda (name)
+;;     "Return non-nil if buffer NAME looks like a javascript file."
+;;     (string-match-p "\\.js\\b" name)))
 
-(defalias 'ric/ivy--elisp-p
-  (lambda (name)
-    "Return non-nil if buffer NAME looks like an emacs lisp file."
-    (string-match-p "\\.el\\b" name)))
+;; (defalias 'ric/ivy--elisp-p
+;;   (lambda (name)
+;;     "Return non-nil if buffer NAME looks like an emacs lisp file."
+;;     (string-match-p "\\.el\\b" name)))
 
-(defun ric/ivy-switch-buffer-transformer (str)
-  "Transform candidate STR when switching buffer."
-  (cond
-   ((ric/ivy--term-p str)  (propertize str 'face 'term-color-green))
-   ((ric/ivy--ruby-p str)  (propertize str 'face 'term-color-red))
-   ((ric/ivy--js-p str)    (propertize str 'face 'term-color-yellow))
-   ((ric/ivy--elisp-p str) (propertize str 'face 'term-color-magenta))
-   (t str)
-   ))
+;; (defun ric/ivy-switch-buffer-transformer (str)
+;;   "Transform candidate STR when switching buffer."
+;;   (cond
+;;    ((ric/ivy--term-p str)  (propertize str 'face 'term-color-green))
+;;    ((ric/ivy--ruby-p str)  (propertize str 'face 'term-color-red))
+;;    ((ric/ivy--js-p str)    (propertize str 'face 'term-color-yellow))
+;;    ((ric/ivy--elisp-p str) (propertize str 'face 'term-color-magenta))
+;;    (t str)
+;;    ))
