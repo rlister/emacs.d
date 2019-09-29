df8ba2c3dc86680e71cd825b4999794dc5f9596f
Richard Lister <rlister@gmail.com>
2019-09-29 15:59:33 -0400

    ensure aliases are set

--- lisp/link-hint-cfg.el
+++ lisp/link-hint-cfg.el
@@ -1,6 +1,7 @@
 (use-package link-hint
   :ensure t
   :init
-  (defalias 'lc 'link-hint-copy-link)
-  (defalias 'lo 'link-hint-open-link)
   )
+
+(defalias 'lc 'link-hint-copy-link)
+(defalias 'lo 'link-hint-open-link)
