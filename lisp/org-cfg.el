56e53f127e072ab72a7b581de1ea1983a76c690b
Richard Lister <rlister@gmail.com>
2019-12-01 16:24:55 -0500

    cleanup agenda and some unused stuff

--- lisp/org-cfg.el
+++ lisp/org-cfg.el
@@ -8,28 +8,8 @@
         org-refile-targets '(
                              (nil :maxlevel . 1)
                              ("~/doc/journalx.org" :maxlevel . 3)
+                             ("~/doc/home.org"     :maxlevel . 3)
                              )
-        org-agenda-files '(
-                           "~/doc/journalx.org"
-                           )
-        org-agenda-custom-commands '(
-                                     ("w" "Agenda and work tasks"
-                                      ((agenda "" ((org-agenda-span 7)
-                                                   (org-agenda-start-on-weekday nil)))
-                                       (tags-todo "work")))
-                                     ("h" "Agenda and home tasks"
-                                      ((agenda "" ((org-agenda-span 7)
-                                                   (org-agenda-start-on-weekday nil)))
-                                       (tags-todo "home")))
-                                     ;; ((org-agenda-prefix-format " %i %-12:c %l"))
-                                     ;; )))
-                                     ;; (org-agenda-prefix-format)
-                                     ;; Its value is
-                                     ;; ((agenda . " %i %-12:c%?-12t% s")
-                                     ;;  (todo . " %i %-12:c")
-                                     ;;  (tags . " %i %-12:c")
-                                     ;;  (search . " %i %-12:c"))
-                                     )
         org-fontify-done-headline t
         org-todo-keywords '("TODO" "WAIT" "WIP" "|" "DONE" "CANCELLED")
         org-todo-interpretation 'sequence
@@ -45,35 +25,26 @@
           ;; ("DONE" .      (:foreground "ForestGreen"    :weight 'normal))
           )
         org-log-done 'time
-        org-agenda-show-log t
         )
   :config
   (setq auto-mode-alist (rassq-delete-all 'dcl-mode auto-mode-alist)) ;no dcl for .com files
   :bind (
          :map org-mode-map
-         ("C-j" . other-window)    ;do not steal this
-         ("C-k" . prev-window)    ;do not steal this
          ("C-," . ric/switch-project)
-         ;; ("C-c d" . org-cut-subtree)
-         ;; ("C-c a" . org-agenda)
          ;; ("C-c p" . ric/org-convert-todo-to-project)
          ([C-return] . switch-to-buffer)
-         :map global-map
-         ;; ("M-i" . ric/org-inbox)
-         ("H-a" . org-agenda)
-         ("C-c a" . org-agenda)
         )
   )
 
-(defun ric/org-convert-todo-to-project ()
-    "Convert a TODO item to a project with subheadings."
-    (interactive)
-    (org-todo "")
-    (end-of-line)
-    (insert " [/]")
-    (open-line 1)
-    (org-insert-todo-subheading nil)
-  )
+;; (defun ric/org-convert-todo-to-project ()
+;;     "Convert a TODO item to a project with subheadings."
+;;     (interactive)
+;;     (org-todo "")
+;;     (end-of-line)
+;;     (insert " [/]")
+;;     (open-line 1)
+;;     (org-insert-todo-subheading nil)
+;;   )
 
 (defun om ()
   "Insert org magic at point and turn on org-mode."
@@ -81,12 +52,3 @@
   (beginning-of-buffer)
   (insert "-*- mode: org; -*-\n\n")
   (org-mode))
-
-
-(defun ric/org-inbox ()
-  "Jump to GTD inbox."
-  (interactive)
-  (find-file "~/notes/gtd/inbox.org"))
-
-;; (define-key org-agenda-mode-map (kbd "M->") 'org-agenda-later)
-;; (define-key org-agenda-mode-map (kbd "M-<") 'org-agenda-earlier)
