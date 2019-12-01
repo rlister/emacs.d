13ecc28d4b274f510d1f9a9a4434bdf065f6161e
Richard Lister <rlister@gmail.com>
2019-12-01 16:01:53 -0500

    colemak e binding

--- lisp/rg-cfg.el
+++ lisp/rg-cfg.el
@@ -1,6 +1,10 @@
 (use-package rg
   :ensure t
-  :bind
-  ("H-r" . rg)
-  ("C-M-s-r" . rg)
+  :bind (
+         :map global-map
+         ("H-r" . rg)
+         ("C-M-s-r" . rg)
+         :map rg-mode-map
+         ("e" . previous-error-no-select)
+         )
   )
