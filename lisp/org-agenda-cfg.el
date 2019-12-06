a551a0f5112a74c9aa491fe9a5f5f3b08acc3a4a
Richard Lister <rlister@gmail.com>
2019-12-05 20:40:15 -0500

    agenda locations

--- lisp/org-agenda-cfg.el
+++ lisp/org-agenda-cfg.el
@@ -1,33 +1,28 @@
 (use-package org-agenda
   :init
-  (setq org-agenda-files '(
-                           "~/doc/journalx.org"
-                           "~/doc/home.org"
-                           )
-        org-agenda-custom-commands '(
-                                     ("w" "Agenda and work tasks"
-                                      ((agenda "" ((org-agenda-span 7)
-                                                   (org-agenda-start-on-weekday nil)))
-                                       (tags-todo "work")))
-                                     ("h" "Agenda and home tasks"
-                                      ((agenda "" ((org-agenda-span 7)
-                                                   (org-agenda-start-on-weekday nil)))
-                                       (tags-todo "home")))
-                                     ;; ((org-agenda-prefix-format " %i %-12:c %l"))
-                                     ;; )))
-                                     ;; (org-agenda-prefix-format)
-                                     ;; Its value is
-                                     ;; ((agenda . " %i %-12:c%?-12t% s")
-                                     ;;  (todo . " %i %-12:c")
-                                     ;;  (tags . " %i %-12:c")
-                                     ;;  (search . " %i %-12:c"))
-                                     )
-        org-agenda-show-log t
-        org-agenda-log-mode-items '(closed clock state)
+  (setq org-agenda-files '("~/doc/work.org" "~/doc/home.org"))
+  (setq org-agenda-custom-commands
+        '(
+          ("c" "Composite agenda and todo"
+           ((agenda "")
+            (alltodo)))
+          ("h" "Home tasks"
+           agenda ""
+           ((org-agenda-files '("~/doc/home.org"))
+            (org-agenda-text-search-extra-files nil)))
+          ("w" "Work tasks"
+           agenda ""
+           ((org-agenda-files '("~/doc/work.org"))
+            (org-agenda-text-search-extra-files nil)))
+          )
         )
+  (setq org-agenda-show-log t)
+  (setq org-agenda-log-mode-items '(closed clock state))
   :bind (
          :map org-agenda-mode-map
-         ("e" . org-agenda-previous-line)
+         ("e" . org-agenda-previous-line) ;colemak
+         ("j" . org-agenda-next-line)     ;qwerty
+         ("k" . org-agenda-previous-line) ;qwerty
          :map global-map
          ;; ("M-i" . ric/org-inbox)
          ("H-a" . org-agenda)
