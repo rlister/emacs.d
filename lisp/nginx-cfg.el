a4f6740484586bad3ebf15617b0d9f017902d228
Richard Lister <rlister@gmail.com>
2022-02-01 13:51:34 -0500

    unused

--- lisp/nginx-cfg.el
+++
@@ -1,8 +0,0 @@
-(use-package nginx-mode
-  :ensure t
-  :mode "config/nginx/.*"
-  :config
-  (setq nginx-indent-level 2)
-  ;; (add-to-list 'auto-mode-alist
-  ;;              '("config/nginx/.*" . nginx-mode))
-  )
