72bc7fc189fab507897fe8aa01282cf8a0018fac
Richard Lister <rlister@gmail.com>
2022-03-05 17:34:08 -0500

    use this a lot more

--- ric-keys-mode.el
+++ ric-keys-mode.el
@@ -23,7 +23,7 @@
     (define-key map (kbd "C-= g") #'slack-group-select)
     (define-key map (kbd "C-= i") #'slack-im-select)
     (define-key map (kbd "C-= u") #'slack-select-unread-rooms)
-    (define-key map (kbd "M-\\") #'mark-sexp)
+    (define-key map (kbd "C-\\") #'mark-sexp)
     (define-key map (kbd "C-c L") #'link-hint-copy-link)
     (define-key map (kbd "C-c c") #'org-capture)
     (define-key map (kbd "C-c e") #'eshell)
