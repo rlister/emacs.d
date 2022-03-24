34d240d17855eb9a880ab188a4dc93ede3f42146
Richard Lister <rlister@gmail.com>
2022-03-24 13:10:06 -0400

    moved into lib

--- unfill-paragraph.el
+++
@@ -1,8 +0,0 @@
-(defun unfill-paragraph (&optional region)
-  "Opposite of fill-paragraph."
-  (interactive (progn (barf-if-buffer-read-only) '(t)))
-  (let ((fill-column (point-max))
-        (emacs-lisp-docstring-fill-column t))
-    (fill-paragraph nil region)))
-
-(define-key global-map (kbd "M-Q") 'unfill-paragraph)
