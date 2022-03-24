c5f0137926dbc935fc002002965b10eab5768570
Richard Lister <rlister@gmail.com>
2022-03-24 10:23:55 -0400

    midnight-mode

--- init.el
+++ init.el
@@ -201,3 +201,4 @@
 (add-hook 'server-after-make-frame-hook #'ric/key-translations)
 (run-with-idle-timer 5 nil #'global-visible-mark-mode)
 (run-with-idle-timer 10 nil #'pixel-scroll-mode)
+(run-with-idle-timer 60 nil #'midnight-mode)
