b0a955973c6eee23d034bb65439c06a2bbddc43b
Ric Lister <rlister@gmail.com>
2022-03-25 01:12:25 -0400

    unused

--- lisp/insert-date-time.el
+++
@@ -1,9 +0,0 @@
-(defun date ()
-  "Insert today's date at point."
-  (interactive)
-  (insert (format-time-string "%Y-%m-%d")))
-
-(defun time ()
-  "Insert current time at point."
-  (interactive)
-  (insert (format-time-string "%H:%M:%S")))
