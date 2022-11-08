7ecb8512657b74e8740b509320206b140cc7a63a
Richard Lister <rlister@gmail.com>
2022-11-08 17:22:33 -0500

    use month tree and empty-lines default

--- init.el
+++ init.el
@@ -166,10 +166,10 @@
 
 (with-eval-after-load 'org-capture
   (setq org-capture-templates
-        '(("t" "Todo"  entry (file+olp+datetree "work.org") "* TODO %?\n%i" :empty-lines 0 :tree-type week)
-          ("m" "Mtg"   entry (file+olp+datetree "work.org") "* MTG %?" :empty-lines 0 :tree-type week)
-          ("h" "Home"  entry (file+olp+datetree "home.org") "* TODO %?" :empty-lines 0 :tree-type week)
-          ("s" "Music" entry (file+olp+datetree "music.org") "* TODO %?" :empty-lines 0 :tree-type week))))
+        '(("t" "Todo"  entry (file+olp+datetree "work.org") "* TODO %?\n%i")
+          ("m" "Mtg"   entry (file+olp+datetree "work.org") "* MTG %?")
+          ("h" "Home"  entry (file+olp+datetree "home.org") "* TODO %?")
+          ("s" "Music" entry (file+olp+datetree "music.org") "* TODO %?"))))
 
 (with-eval-after-load 'eshell
   (setq eshell-banner-message ""))
