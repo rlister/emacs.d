021d0d2efcb3a26c1523b3e26599bafac1c816e8
Richard Lister <rlister@gmail.com>
2022-11-08 12:05:05 -0500

    not needed

--- init-org-capture.el
+++ init-org-capture.el
@@ -1,5 +1,3 @@
-(setq org-capture-bookmark nil)
-
 (setq org-capture-templates
       '(("t" "Todo"  entry (file+olp+datetree "work.org") "* TODO %?\n%i" :empty-lines 0 :tree-type week)
         ("m" "Mtg"   entry (file+olp+datetree "work.org") "* MTG %?" :empty-lines 0 :tree-type week)
