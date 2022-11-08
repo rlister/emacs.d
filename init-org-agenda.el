e025449fc79fa8587e27ed878c1b3ab8d873383e
Richard Lister <rlister@gmail.com>
2022-11-08 11:06:16 -0500

    help compiler

--- init-org-agenda.el
+++ init-org-agenda.el
@@ -1,12 +1,10 @@
-(add-hook 'org-agenda-mode-hook 'hl-line-mode)
+(add-hook 'org-agenda-mode-hook #'hl-line-mode)
 (add-hook 'org-agenda-after-show-hook #'org-narrow-to-subtree)
 
 (setq org-agenda-window-setup 'current-window)
-
 (setq org-agenda-custom-commands
       '(("b" "Backlog" alltodo "" ((org-agenda-files '("work.org"))))
         ("g" "Report" agenda "" ((org-agenda-files '("work.org")) (org-agenda-prefix-format "")))
         ("h" "Home" ((agenda "") (alltodo "")) ((org-agenda-files '("home.org"))))
         ("n" "Next" tags "next" ((org-agenda-files '("work.org"))))
         ("w" "Work" agenda "" ((org-agenda-files '("work.org"))))))
-
