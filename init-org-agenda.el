98bc80b375fc985b17f296d5e383d451e540b758
Richard Lister <rlister@gmail.com>
2022-11-08 11:05:38 -0500

    simplify commands

--- init-org-agenda.el
+++ init-org-agenda.el
@@ -4,33 +4,11 @@
 (setq org-agenda-window-setup 'current-window)
 
 (setq org-agenda-custom-commands
-      '(("g" "Geekbot"
-         agenda ""
-         ((org-agenda-files '("~/src/doc/work.org"))
-          (org-agenda-overriding-header "Standup")
-          (org-agenda-show-log t)
-          (org-agenda-todo-keyword-format "- %-4s")
-          (org-agenda-remove-tags t)
-          (org-agenda-prefix-format "")))
-        ("c" "Composite agenda and todo"
-         ((agenda "")
-          (alltodo)))
-        ("h" "Home tasks"
-         ((agenda "" ((org-agenda-files '("~/src/doc/home.org"))))
-          (alltodo "" ((org-agenda-files '("~/src/doc/home.org"))))))
-        ("s" "Music"
-         ((agenda "" ((org-agenda-files '("~/src/doc/music.org"))))
-          (alltodo "" ((org-agenda-files '("~/src/doc/music.org"))))))
-        ("w" "Work tasks"
-         ((agenda "" ((org-agenda-files '("~/src/doc/work.org"))))
-          (alltodo "" ((org-agenda-files '("~/src/doc/work.org"))))))
-        ;; ("w" "Work tasks"
-        ;;  ((agenda ""
-        ;;           ((org-agenda-files '("~/src/doc/work.org" "~/.org-jira/"))
-        ;;            (org-agenda-text-search-extra-files nil)))
-        ;;   (alltodo ""
-        ;;            ((org-agenda-files '("~/.org-jira/"))))))
-        ))
+      '(("b" "Backlog" alltodo "" ((org-agenda-files '("work.org"))))
+        ("g" "Report" agenda "" ((org-agenda-files '("work.org")) (org-agenda-prefix-format "")))
+        ("h" "Home" ((agenda "") (alltodo "")) ((org-agenda-files '("home.org"))))
+        ("n" "Next" tags "next" ((org-agenda-files '("work.org"))))
+        ("w" "Work" agenda "" ((org-agenda-files '("work.org"))))))
 
 (define-key org-agenda-mode-map (kbd "C-<down>") #'org-agenda-next-date-line)
 (define-key org-agenda-mode-map (kbd "C-<up>") #'org-agenda-previous-date-line)
