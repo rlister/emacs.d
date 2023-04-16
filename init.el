964041dd279a945e56707e0e6936ece889e292c0
Richard Lister <rlister@gmail.com>
2023-04-16 11:48:09 -0400

    sort

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq avy-keys '(?t ?n ?s ?e ?r ?i ?a ?o ?f ?u ?d ?h ?l ?p ?g ?m ?c))
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq bookmark-save-flag 1)              ;save after any modification
 (setq clean-buffer-list-delay-general 1) ;midnight-mode days
@@ -121,21 +122,6 @@
 
 (put 'narrow-to-region 'disabled nil)
 
-(with-eval-after-load 'go-mode
-  ;; (add-hook 'go-mode-hook #'tree-sitter-hl-mode)
-  (add-hook 'go-mode-hook #'eglot-ensure)
-  (add-hook 'go-mode-hook #'subword-mode))
-
-(with-eval-after-load 'terraform-mode
-  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
-
-(with-eval-after-load 'eglot
-  (define-key eglot-mode-map (kbd "C-c C-r") #'eglot-rename))
-
-(with-eval-after-load 'flymake
-  (define-key flymake-mode-map (kbd "C-c C-p") #'flymake-goto-prev-error)
-  (define-key flymake-mode-map (kbd "C-c C-n") #'flymake-goto-next-error))
-
 (with-eval-after-load 'dired
   (setq dired-guess-shell-alist-user '(("\\." "xdg-open")))
   (setq dired-listing-switches "-alh")
@@ -145,12 +131,91 @@
   (define-key dired-mode-map (kbd "_") (lambda () (interactive) (dired-do-rename-regexp "^.*$" "_\\&"))) ;prepend underscore
   (define-key dired-mode-map (kbd "C-c _") (lambda () (interactive) (dired-do-rename-regexp "^_" ""))))  ;remove leading underscore
 
+(with-eval-after-load 'eglot
+  (keymap-set eglot-mode-map "C-c C-r" #'eglot-rename))
+
+(with-eval-after-load 'elfeed
+  (elfeed-load-opml "~/src/doc/elfeed.opml")
+  (setq elfeed-search-filter "@2-weeks-ago +unread")
+  (define-key elfeed-search-mode-map "d" #'elfeed-search-untag-all-unread)
+  (define-key elfeed-show-mode-map "d" #'elfeed-kill-buffer)
+  (define-key elfeed-show-mode-map "r" #'elfeed-kill-buffer))
+
+(with-eval-after-load 'flymake
+  (define-key flymake-mode-map (kbd "C-c C-p") #'flymake-goto-prev-error)
+  (define-key flymake-mode-map (kbd "C-c C-n") #'flymake-goto-next-error))
+
+(with-eval-after-load 'forge
+  (define-key forge-topic-mode-map (kbd "r") #'forge-edit-topic-review-requests)
+  (define-key forge-topic-mode-map (kbd "w") #'forge-browse-topic))
+
+(with-eval-after-load 'gcalcli-mode
+  (setq gcalcli-bin "~/.local/bin/gcalcli")
+  (setq gcalcli-config-alist
+        '((home . "~/.config/gcalcli/home")
+          (work . "~/.config/gcalcli/work"))))
+
+(with-eval-after-load 'go-mode
+  ;; (add-hook 'go-mode-hook #'tree-sitter-hl-mode)
+  (add-hook 'go-mode-hook #'eglot-ensure)
+  (add-hook 'go-mode-hook #'subword-mode))
 (with-eval-after-load 'isearch
   (setq isearch-lax-whitespace t)       ;space matches any non-word
   (setq search-whitespace-regexp ".*?") ;very loose matching
   (define-key isearch-mode-map (kbd "C-<return>") 'ric-isearch-exit-other-end)
   (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char) ;backspace should edit search string
   (global-anzu-mode 1))
+
+(with-eval-after-load 'magit
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+  (defun forge--contrast-color (color) "black"))
+
+(with-eval-after-load 'markdown-mode
+  (add-hook 'markdown-mode-hook #'visual-line-mode)
+  (define-key markdown-mode-map (kbd "C-c v") #'markdown-view-mode)
+  (define-key markdown-view-mode-map (kbd "C-c v") #'markdown-mode))
+
+;; (with-eval-after-load 'org-capture
+;;   (add-hook 'org-capture-mode-hook #'pabbrev-mode))
+
+(with-eval-after-load 'org
+  ;; (define-key pabbrev-mode-map [tab] 'pabbrev-expand-maybe) ;https://lists.gnu.org/archive/html/emacs-orgmode/2016-02/msg00311.html
+  (setq org-directory "~/src/doc")
+  (setq org-log-done 'time)
+  (setq org-src-window-setup 'current-window)
+  (setq org-todo-interpretation 'sequence)
+  (setq org-todo-keyword-faces '(("BLOCK" . link) ("REVIEW" . warning) ("WIP" . success)))
+  (setq org-todo-keywords '("TODO" "BLOCK(b@/!)" "REVIEW(r@/!)" "WIP(w!)" "|" "DONE(d!)" "CANCELLED(c@)")))
+
+(with-eval-after-load 'org-agenda
+  (defun org-agenda-set-mode-name ()
+    (setq mode-name (list "OrgAgenda")))
+  (setq org-agenda-window-setup 'current-window)
+  (setq org-agenda-custom-commands
+        '(("b" "Backlog" alltodo "" ((org-agenda-files '("work.org"))))
+          ("g" "Report" agenda "" ((org-agenda-files '("work.org")) (org-agenda-prefix-format "")))
+          ("h" "Home" ((agenda "") (alltodo "")) ((org-agenda-files '("home.org"))))
+          ("n" "Next" tags "next" ((org-agenda-files '("work.org"))))
+          ("w" "Work" agenda "" ((org-agenda-files '("work.org"))))))
+  (add-hook 'org-agenda-mode-hook #'hl-line-mode)
+  (add-hook 'org-agenda-after-show-hook #'org-narrow-to-subtree)
+  (define-key org-agenda-mode-map (kbd "<C-down>") #'org-agenda-next-date-line)
+  (define-key org-agenda-mode-map (kbd "<C-up>") #'org-agenda-previous-date-line))
+
+(with-eval-after-load 'org-capture
+  (add-hook 'org-capture-mode-hook #'pabbrev-mode)
+  (setq org-capture-templates
+        '(("t" "Todo"  entry (file+olp+datetree "work.org") "* TODO %?\n%i")
+          ("m" "Mtg"   entry (file+olp+datetree "work.org") "* MTG %?")
+          ("h" "Home"  entry (file+olp+datetree "home.org") "* TODO %?")
+          ("s" "Music" entry (file+olp+datetree "music.org") "* TODO %?"))))
+
+(with-eval-after-load 'pabbrev
+  ;; (put 'org-mode 'pabbrev-global-mode-excluded-modes t) ;org-mode has post-command-hook bug with pabbrev
+  (define-key pabbrev-mode-map [tab] 'pabbrev-expand-maybe)) ;https://lists.gnu.org/archive/html/emacs-orgmode/2016-02/msg00311.html
+
+(with-eval-after-load 'package
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 
 (with-eval-after-load 'project
   (defun ric-project-vterm ()
@@ -167,58 +232,12 @@
           (ric-project-vterm "vterm" ?t)
           (vc-dir "vc" ?v))))
 
-(with-eval-after-load 'package
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+;; (with-eval-after-load 'smerge-mode
+;;   (define-key smerge-mode-map (kbd "C-<down>") #'smerge-next)
+;;   (define-key smerge-mode-map (kbd "C-<up>") #'smerge-prev))
 
-(with-eval-after-load 'magit
-  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
-  (defun forge--contrast-color (color) "black"))
-
-(with-eval-after-load 'forge
-  (define-key forge-topic-mode-map (kbd "r") #'forge-edit-topic-review-requests)
-  (define-key forge-topic-mode-map (kbd "w") #'forge-browse-topic))
-
-(with-eval-after-load 'smerge-mode
-  (define-key smerge-mode-map (kbd "C-<down>") #'smerge-next)
-  (define-key smerge-mode-map (kbd "C-<up>") #'smerge-prev))
-
-(with-eval-after-load 'avy
-  (setq avy-keys '(?t ?n ?s ?e ?r ?i ?a ?o ?f ?u ?d ?h ?l ?p ?g ?m ?c))) ;colemak-friendly keys
-
-(with-eval-after-load 'pabbrev
-  (put 'org-mode 'pabbrev-global-mode-excluded-modes t) ;org-mode has post-command-hook bug with pabbrev
-  (define-key pabbrev-mode-map [tab] 'pabbrev-expand-maybe)) ;https://lists.gnu.org/archive/html/emacs-orgmode/2016-02/msg00311.html
-
-(with-eval-after-load 'org
-  (define-key pabbrev-mode-map [tab] 'pabbrev-expand-maybe) ;https://lists.gnu.org/archive/html/emacs-orgmode/2016-02/msg00311.html
-  (setq org-directory "~/src/doc")
-  (setq org-log-done 'time)
-  (setq org-src-window-setup 'current-window)
-  (setq org-todo-interpretation 'sequence)
-  (setq org-todo-keyword-faces '(("BLOCK" . link) ("REVIEW" . warning) ("WIP" . success)))
-  (setq org-todo-keywords '("TODO" "BLOCK(b@/!)" "REVIEW(r@/!)" "WIP(w!)" "|" "DONE(d!)" "CANCELLED(c@)")))
-
-(with-eval-after-load 'org-agenda
-  (defun org-agenda-set-mode-name ()
-    (setq mode-name (list "Org-Agenda")))
-  (setq org-agenda-window-setup 'current-window)
-  (setq org-agenda-custom-commands
-        '(("b" "Backlog" alltodo "" ((org-agenda-files '("work.org"))))
-          ("g" "Report" agenda "" ((org-agenda-files '("work.org")) (org-agenda-prefix-format "")))
-          ("h" "Home" ((agenda "") (alltodo "")) ((org-agenda-files '("home.org"))))
-          ("n" "Next" tags "next" ((org-agenda-files '("work.org"))))
-          ("w" "Work" agenda "" ((org-agenda-files '("work.org"))))))
-  (add-hook 'org-agenda-mode-hook #'hl-line-mode)
-  (add-hook 'org-agenda-after-show-hook #'org-narrow-to-subtree)
-  (define-key org-agenda-mode-map (kbd "<C-down>") #'org-agenda-next-date-line)
-  (define-key org-agenda-mode-map (kbd "<C-up>") #'org-agenda-previous-date-line))
-
-(with-eval-after-load 'org-capture
-  (setq org-capture-templates
-        '(("t" "Todo"  entry (file+olp+datetree "work.org") "* TODO %?\n%i")
-          ("m" "Mtg"   entry (file+olp+datetree "work.org") "* MTG %?")
-          ("h" "Home"  entry (file+olp+datetree "home.org") "* TODO %?")
-          ("s" "Music" entry (file+olp+datetree "music.org") "* TODO %?"))))
+(with-eval-after-load 'terraform-mode
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
 
 (with-eval-after-load 'vterm
   (setq vterm-buffer-name-string "*vterm %s*") ;include shell title in buffer namen
@@ -229,24 +248,6 @@
   ;; (define-key vterm-mode-map (kbd "C-c C-c") #'vterm--self-insert)
   (define-key vterm-mode-map (kbd "<C-return>") #'vterm-copy-mode)
   (define-key vterm-copy-mode-map (kbd "<C-return>") #'vterm-copy-mode))
-
-(with-eval-after-load 'elfeed
-  (elfeed-load-opml "~/src/doc/elfeed.opml")
-  (setq elfeed-search-filter "@2-weeks-ago +unread")
-  (define-key elfeed-search-mode-map "d" #'elfeed-search-untag-all-unread)
-  (define-key elfeed-show-mode-map "d" #'elfeed-kill-buffer)
-  (define-key elfeed-show-mode-map "r" #'elfeed-kill-buffer))
-
-(with-eval-after-load 'gcalcli-mode
-  (setq gcalcli-bin "~/.local/bin/gcalcli")
-  (setq gcalcli-config-alist
-        '((home . "~/.config/gcalcli/home")
-          (work . "~/.config/gcalcli/work"))))
-
-(with-eval-after-load 'markdown-mode
-  (add-hook 'markdown-mode-hook #'visual-line-mode)
-  (define-key markdown-mode-map (kbd "C-c v") #'markdown-view-mode)
-  (define-key markdown-view-mode-map (kbd "C-c v") #'markdown-mode))
 
 (autoload 'mu4e "mu4e" nil t)
 (with-eval-after-load 'mu4e
