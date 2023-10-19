97a730206200a12f6aba594abfd6f3810d2e9d9f
Richard Lister <rlister@gmail.com>
2023-10-19 16:00:56 -0400

    some bad ts stuff

--- init.el
+++ init.el
@@ -85,11 +85,6 @@
 
 ;; (add-to-list 'auto-mode-alist '("Envfile" . ruby-mode))
 ;; (add-to-list 'auto-mode-alist '("Staxfile" . ruby-mode))
-;; (add-to-list 'auto-mode-alist '("\\.\\(ya?ml\\)\\'" . yaml-mode))
-;; (add-to-list 'auto-mode-alist '("\\.\\(e?ya?\\|ra\\)ml\\'" . yaml-ts-mode))
-;; (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
-;; (add-to-list 'auto-mode-alist '("\\.dockerfile\\'" . dockerfile-ts-mode))
-(add-to-list 'auto-mode-alist '("\\(?:\\.\\(?:rbw?\\|ru\\|rake\\|gemspec\\)\\|/\\(?:Env\\|Gem\\|Rake\\|Stax\\)file\\)\\'" . ruby-ts-mode))
 
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
