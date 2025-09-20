d7ff4252dcbded9cb6909846d20cc299644effc2
Richard Lister <rlister@gmail.com>
2025-09-20 13:21:51 -0400

    cleanup various mu4e experiments

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -1,12 +1,21 @@
+;; (setq mu4e-read-option-use-builtin nil)
 (setq mu4e-change-filenames-when-moving t) ;important for mbsync
-(setq mu4e-sent-messages-behavior 'delete) ;do not save to Sent: gmail imap takes care of this
-(setq mu4e-context-policy 'pick-first)     ;start with default context
-(setq mu4e-headers-thread-last-child-prefix '(" >" . "└ ")) ;nicer ascii char
-(setq mu4e-headers-fields '((:human-date . 12) (:flags . 6) (:maildir . 15) (:mailing-list . 10) (:from-or-to . 22) (:subject)))
-
-;; (setq mu4e-split-view 'single-window)
-
+(setq mu4e-completing-read-function 'completing-read)
 (setq mu4e-compose-format-flowed t)
+(setq mu4e-context-policy 'pick-first)     ;start with default context
+(setq mu4e-sent-messages-behavior 'delete) ;do not save to Sent: gmail imap takes care of this
+
+(define-key mu4e-headers-mode-map (kbd "i") #'mu4e-update-index)
+(define-key mu4e-main-mode-map (kbd "i") #'mu4e-update-index)
+(define-key mu4e-view-mode-map (kbd "<backspace>") #'scroll-down)
+
+(add-hook 'mu4e-view-mode-hook #'visual-line-mode)
+
+;; (setq mu4e-headers-thread-last-child-prefix '(" >" . "└ ")) ;nicer ascii char
+
+;; (setq mu4e-headers-fields '((:human-date . 12) (:flags . 6) (:maildir . 15) (:mailing-list . 10) (:from-or-to . 22) (:subject)))
+;; (setq mu4e-headers-fields '((:human-date . 12) (:flags . 6) (:mailing-list . 10) (:from . 22)
+;;  (:subject)))
 
 (setq mu4e-contexts
       `(,(make-mu4e-context
@@ -18,7 +27,9 @@
                   (mu4e-refile-folder . "/home/archive")
                   (mu4e-trash-folder . "/home/trash")
                   (mu4e-drafts-folder . "/home/drafts")
-                  (mu4e-maildir-shortcuts . ((:maildir "/home/inbox" :key ?i) (:maildir "/home/spam" :key ?s) (:maildir "/home/sent" :key ?S) (:maildir "/home/trash" :key ?t)))))
+                  (mu4e-maildir-shortcuts . ((:maildir "/home/inbox" :key ?i) (:maildir "/home/spam" :key ?s)))
+                  ;; (mu4e-maildir-shortcuts . ((:maildir "/home/inbox" :key ?i) (:maildir "/home/spam" :key ?s) (:maildir "/home/sent" :key ?S) (:maildir "/home/trash" :key ?t)))
+                  ))
         ,(make-mu4e-context
           :name "work"
           :match-func (lambda (m) (when m (string-match-p "^/work" (mu4e-message-field m :maildir))))
@@ -28,16 +39,6 @@
                   (mu4e-refile-folder . "/work/archive")
                   (mu4e-trash-folder . "/work/trash")
                   (mu4e-drafts-folder . "/work/drafts")
-                  (mu4e-maildir-shortcuts . ((:maildir "/work/inbox" :key ?i) (:maildir "/work/spam" :key ?s) (:maildir "/work/sent" :key ?S) (:maildir "/work/trash" :key ?t)))
-))))
-
-(define-key mu4e-main-mode-map (kbd "i") #'mu4e-update-index)
-(define-key mu4e-headers-mode-map (kbd "i") #'mu4e-update-index)
-
-;; (define-key mu4e-headers-mode-map (kbd "c") #'mu4e-context-switch)
-(define-key mu4e-view-mode-map (kbd "<backspace>") #'scroll-down)
-
-;; borken
-;; (advice-add 'mu4e-view-mark-for-trash :after #'kill-current-buffer)
-
-(add-hook 'mu4e-view-mode-hook #'visual-line-mode)
+                  (mu4e-maildir-shortcuts . ((:maildir "/work/inbox" :key ?i) (:maildir "/work/spam" :key ?s)))
+                  ;; (mu4e-maildir-shortcuts . ((:maildir "/work/inbox" :key ?i) (:maildir "/work/spam" :key ?s) (:maildir "/work/sent" :key ?S) (:maildir "/work/trash" :key ?t)))
+                  ))))
